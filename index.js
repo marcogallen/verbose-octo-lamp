c859c6b3323c9177ae7b345a64b9e9d804c62eed
Marco Gallen <marco.gallen@gmail.com>
2021-05-27 17:24:51 +1000

    Adding more files

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 #!/usr/bin/env node
 const prog = require('caporal');
+const createCmd = require('./lib/create');
 
 prog
 .version('1.0.0')
 .command('create', 'Create a new application')
 .argument('<template>','Template to use')
 .option('--variant <variant>', 'Which <variant> of template is going to be created')
-.action((args, options, logger) => {
-    console.log({
-        args: args,
-        options: options
-    });
-});
+.action(createCmd);
 
 prog.parse(process.argv);
